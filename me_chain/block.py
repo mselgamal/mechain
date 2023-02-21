fe4217795f212d7b741cc008011aa76f29f5f7e4
Mamdouh Elgamal <mselgamal2@gmail.com>
2023-02-21 13:17:38 -0500

    feat: add method to create block and get last block

--- me_chain/block.py
+++ me_chain/block.py
@@ -3,11 +3,15 @@
 """
 
 class Block:
-    def __init__(self) -> None:
+    def __init__(self, index: int, timestamp: str, proof: int, prev_hash: str) -> None:
         self.nonce = 0
-        self.id = 0
-        self.data = None
-        self.hash = None
-        self.prev_hash = None
-        self.prev_block = None
-        self.next_block = None
+        self.index = index
+        self.proof = proof
+        self.timestamp = timestamp
+        self.prev_hash = prev_hash
+    
+    def __repr__(self) -> str:
+        return f"Block(index={self.index}, timestamp={self.timestamp}, proof={self.proof}, prev_hash={self.prev_hash})"
+    
+    def json(self):
+        return {"index": self.index, "timestamp": self.timestamp, "proof": self.proof, "prev_hash": self.prev_hash}
--- me_chain/blockchain.py
+++ me_chain/blockchain.py
@@ -2,12 +2,28 @@
     blockchain class    
 """
 from block import Block
+import hashlib
+import datetime
+import json
 import logging
 
 class Blockchain:
     def __init__(self) -> None:
         self.name = "MEchain"
-        self.head_block:Block  = None
+        self.chain:list[Block] = []
+        self.create_block(proof=1, prev_hash='0')
+    
+    def create_block(self, proof: int, prev_hash: str):
+        self.chain.append(Block(len(self.chain)+1, datetime.datetime.utcnow(), proof, prev_hash))
+            
+    def mine(self):
+        pass
+    
+    def get_last_block(self):
+        return self.chain[-1]
+    
+    def get_proof_of_work(self):
+        pass
     
     def __repr__(self) -> str:
         result = ''
